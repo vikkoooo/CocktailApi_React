d9af65e9fe272db776b9412fabb20d6bc1a15cf3
vikkoooo <vikkolundberg@gmail.com>
2024-08-12 14:56:20 +0200

    should be h1

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -15,7 +15,7 @@ export function HomePage(): ReactElement {
 		<div className="home-page">
 			{cocktail && (
 				<div className="cocktail-card">
-					<h2>{cocktail.strDrink}</h2>
+					<h1>{cocktail.strDrink}</h1>
 					<img src={cocktail.strDrinkThumb} alt={cocktail.strDrink} />
 					<Link to={`/info/${cocktail.idDrink}`} className="link">See more</Link>
 				</div>
