0db4f1614e577708e1e4367cb0ce56a242e622ad
vikkoooo <vikkolundberg@gmail.com>
2024-08-12 14:53:36 +0200

    add :idDrink tag to info path

--- src/router.tsx
+++ src/router.tsx
@@ -6,10 +6,10 @@ import { CocktailInfoPage } from "./pages/CocktailInfoPage";
 
 export const router = createBrowserRouter(
 	createRoutesFromElements(
-		<Route path="/" element={< App />}>
-			<Route index element={< HomePage />} />
-			<Route path="search" element={< SearchPage />} />
-			<Route path="info" element={< CocktailInfoPage />} />
+		<Route path="/" element={<App />}>
+			<Route index element={<HomePage />} />
+			<Route path="search" element={<SearchPage />} />
+			<Route path="info/:idDrink" element={<CocktailInfoPage />} />
 		</Route>
 	)
 );
