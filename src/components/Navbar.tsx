a13879d7ba5e346492cf46af48cb33051e50720f
vikkoooo <vikkolundberg@gmail.com>
2024-08-13 16:05:12 +0200

    final styling of navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,9 +5,10 @@ export function Navbar(): ReactElement {
 	return (
 		<header className="navbar">
 			<Link to="/" className="link-home">Drink Showcase</Link>
-			<span className="spacer" />
-			<Link to="/search" className="link-search">Search</Link>
-			<button type="button" onClick={() => console.log("button-random clicked")} className="button-random">Random Cocktail</button>
+			<div className="navbar-links">
+				<Link to="/search" className="link-search">Search</Link>
+				<button type="button" onClick={() => console.log("button-random clicked")} className="button-random">Random Cocktail</button>
+			</div>
 		</header>
 	);
 }
