2f57caa62d3733471d7db74a6e0413e75aab9dea
vikkoooo <vikkolundberg@gmail.com>
2024-08-13 16:36:20 +0200

    style homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -13,11 +13,12 @@ export function HomePage(): ReactElement {
 
 	return (
 		<div className="home-page">
+			<h1>Featured Drink</h1>
 			{cocktail && (
 				<div className="cocktail-card">
-					<h1>{cocktail.strDrink}</h1>
 					<img src={cocktail.strDrinkThumb} alt={cocktail.strDrink} />
-					<Link to={`/info/${cocktail.idDrink}`} className="link">See more</Link>
+					<h2>{cocktail.strDrink}</h2>
+					<Link to={`/info/${cocktail.idDrink}`} className="link">See More</Link>
 				</div>
 			)}
 			<button type="button" onClick={fetchCocktailRandom}>Reload random cocktail</button>
